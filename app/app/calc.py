dbe31de48af54796ca90dbde2a3887e6aa45e0c8
Czvaibhav <vaibhavksharma54321@gmail.com>
2024-01-22 13:34:32 +0530

    Postgre Updated

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator function
+"""
+
+
+def add(x, y):
+    """Add x and y and return result"""
+    return x+y
+
+def subtract(x,y):
+    """Subtract x and y and return result"""
+    return abs(x-y)
