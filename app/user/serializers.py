f9ae1d5ba2f7666608276e4b18c7eb0e7cf3ec92
Czvaibhav <vaibhavksharma54321@gmail.com>
2024-02-04 20:42:02 +0530

    Updating Code

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -3,16 +3,16 @@ Serializers for the user API VIEW
 """
 from django.contrib.auth import get_user_model
 
-from rest_framework import Serializers
+from rest_framework import serializers
 
 
-class UserSerializer(sereializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
 
     class Meta:
         model = get_user_model()
         fields = ['email', 'password', 'name']
-        extra_kwargs = {'password', {'write_only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return a user with enCrypted password"""
